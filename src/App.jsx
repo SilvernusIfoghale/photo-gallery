f9159e8f1868ddbba822646d4186dd345d1cc9f7
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2024-08-07 18:20:41 +0100

    photoGallery component created and exported to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import PhotoGallery from "./component/PhotoGallery";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <PhotoGallery />;
 }
