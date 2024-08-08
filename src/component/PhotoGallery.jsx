5fc469fc73af88c5366da38565b46b98953282a4
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2024-08-08 15:05:14 +0100

    layout restyled

--- src/component/PhotoGallery.jsx
+++ src/component/PhotoGallery.jsx
@@ -33,7 +33,7 @@ export default function PhotoGallery() {
   return (
     <>
       <div className="w-full relative">
-        <div className="text-center text-5xl font-bold my-10 bg-blue-200 py-8">
+        <div className="text-center text-5xl font-bold mb-10 bg-blue-200 py-8">
           Photo Gallery
         </div>
         {!photos && (
@@ -42,7 +42,7 @@ export default function PhotoGallery() {
         <div className="grid grid-cols-1  sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 xxl:grid-cols-5 place-items-center w-full">
           {photos?.map((photo, index) => (
             <div
-              className="border-4 mx-3 my-7 border-blue-300 h-[420px]  w-[390px] sm:h-[420px] sm:w-[310px] rounded-lg overflow-hidden relative"
+              className="border-4 mx-3 my-7 border-blue-300 h-[420px]  w-[390px] sm:h-[420px] sm:w-[310px] rounded-3xl overflow-hidden relative shadow-lg shadow-gray-300"
               key={index}
             >
               <div className=" h-[315px] w-[395px] sm:h-[315px] sm:w-[375px]  ">
@@ -52,16 +52,16 @@ export default function PhotoGallery() {
                   alt={photo.urls.alt_description}
                 />
               </div>
-              <div className="absolute w-full border-t-2 bottom-0">
-                <div className="rounded-full w-12 h-12 overflow-hidden border-2 ml-2 border-blue-200 mt-3">
+              <div className="absolute w-full  bottom-6 flex items-end ">
+                <div className="rounded-full w-12 h-12 overflow-hidden border-2 ml-2 border-blue-200 mt-3 ">
                   <img
                     className="object-cover w-full h-full"
                     src={photo.user.profile_image.medium}
                   />
                 </div>
-                <div className="flex justify-between pr-4 pl-1 pb-3 ">
-                  <p className="font-semibold italic ">{photo.user.name}</p>
-                  <div className="flex gap-1 items-center">
+                <div className="flex justify-between py-2 w-[80%] ">
+                  <p className="font-semibold italic pl-2">{photo.user.name}</p>
+                  <div className="flex gap-1 items-center pr-2">
                     <div className=" w-5 h-5">
                       <img src={like} alt="likes" />
                     </div>
